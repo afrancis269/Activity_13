f0463270cfcad083200192fd272b340018c54e9e
afrancis269 <96451524+afrancis269@users.noreply.github.com>
2022-01-31 08:22:42 +0700

    Initial commit

---
+++ Activity_13/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Activity_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lbx1 = new System.Windows.Forms.Label();
+            this.Lbx2 = new System.Windows.Forms.Label();
+            this.Lbx3 = new System.Windows.Forms.Label();
+            this.Lbx4 = new System.Windows.Forms.Label();
+            this.Lbx5 = new System.Windows.Forms.Label();
+            this.Lbx6 = new System.Windows.Forms.Label();
+            this.Lbx7 = new System.Windows.Forms.Label();
+            this.Lbx8 = new System.Windows.Forms.Label();
+            this.Lbx9 = new System.Windows.Forms.Label();
+            this.ResTxb = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 0;
+            // 
+            // Lbx1
+            // 
+            this.Lbx1.Location = new System.Drawing.Point(31, 31);
+            this.Lbx1.Name = "Lbx1";
+            this.Lbx1.Size = new System.Drawing.Size(107, 73);
+            this.Lbx1.TabIndex = 1;
+            this.Lbx1.Text = "label2";
+            // 
+            // Lbx2
+            // 
+            this.Lbx2.Location = new System.Drawing.Point(189, 31);
+            this.Lbx2.Name = "Lbx2";
+            this.Lbx2.Size = new System.Drawing.Size(107, 73);
+            this.Lbx2.TabIndex = 2;
+            this.Lbx2.Text = "label3";
+            // 
+            // Lbx3
+            // 
+            this.Lbx3.Location = new System.Drawing.Point(356, 31);
+            this.Lbx3.Name = "Lbx3";
+            this.Lbx3.Size = new System.Drawing.Size(107, 73);
+            this.Lbx3.TabIndex = 3;
+            this.Lbx3.Text = "label4";
+            // 
+            // Lbx4
+            // 
+            this.Lbx4.Location = new System.Drawing.Point(31, 136);
+            this.Lbx4.Name = "Lbx4";
+            this.Lbx4.Size = new System.Drawing.Size(107, 73);
+            this.Lbx4.TabIndex = 4;
+            this.Lbx4.Text = "label5";
+            // 
+            // Lbx5
+            // 
+            this.Lbx5.Location = new System.Drawing.Point(189, 136);
+            this.Lbx5.Name = "Lbx5";
+            this.Lbx5.Size = new System.Drawing.Size(107, 73);
+            this.Lbx5.TabIndex = 5;
+            this.Lbx5.Text = "label6";
+            // 
+            // Lbx6
+            // 
+            this.Lbx6.Location = new System.Drawing.Point(356, 136);
+            this.Lbx6.Name = "Lbx6";
+            this.Lbx6.Size = new System.Drawing.Size(107, 73);
+            this.Lbx6.TabIndex = 6;
+            this.Lbx6.Text = "label7";
+            // 
+            // Lbx7
+            // 
+            this.Lbx7.Location = new System.Drawing.Point(31, 246);
+            this.Lbx7.Name = "Lbx7";
+            this.Lbx7.Size = new System.Drawing.Size(107, 73);
+            this.Lbx7.TabIndex = 7;
+            this.Lbx7.Text = "label8";
+            // 
+            // Lbx8
+            // 
+            this.Lbx8.Location = new System.Drawing.Point(189, 246);
+            this.Lbx8.Name = "Lbx8";
+            this.Lbx8.Size = new System.Drawing.Size(107, 73);
+            this.Lbx8.TabIndex = 8;
+            this.Lbx8.Text = "label9";
+            // 
+            // Lbx9
+            // 
+            this.Lbx9.Location = new System.Drawing.Point(356, 246);
+            this.Lbx9.Name = "Lbx9";
+            this.Lbx9.Size = new System.Drawing.Size(107, 73);
+            this.Lbx9.TabIndex = 9;
+            this.Lbx9.Text = "label10";
+            // 
+            // ResTxb
+            // 
+            this.ResTxb.Location = new System.Drawing.Point(116, 333);
+            this.ResTxb.Name = "ResTxb";
+            this.ResTxb.ReadOnly = true;
+            this.ResTxb.Size = new System.Drawing.Size(204, 30);
+            this.ResTxb.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(116, 380);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 39);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "New Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(221, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 39);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 431);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ResTxb);
+            this.Controls.Add(this.Lbx9);
+            this.Controls.Add(this.Lbx8);
+            this.Controls.Add(this.Lbx7);
+            this.Controls.Add(this.Lbx6);
+            this.Controls.Add(this.Lbx5);
+            this.Controls.Add(this.Lbx4);
+            this.Controls.Add(this.Lbx3);
+            this.Controls.Add(this.Lbx2);
+            this.Controls.Add(this.Lbx1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lbx1;
+        private System.Windows.Forms.Label Lbx2;
+        private System.Windows.Forms.Label Lbx3;
+        private System.Windows.Forms.Label Lbx4;
+        private System.Windows.Forms.Label Lbx5;
+        private System.Windows.Forms.Label Lbx6;
+        private System.Windows.Forms.Label Lbx7;
+        private System.Windows.Forms.Label Lbx8;
+        private System.Windows.Forms.Label Lbx9;
+        private System.Windows.Forms.TextBox ResTxb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Activity_13/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_13
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int win_check(int[,] arg)
+        {
+            bool Oplayer = false, Xplayer = false;
+            //check column
+            for (int i = 0; i < 3; i++)
+            {
+                if ((arg[0, i] == arg[1, i]) && (arg[1, i] == arg[2, i]))
+                {
+                    if (arg[2, i] == 0)
+                    {
+                        Oplayer = true;
+                    }
+                    else
+                    {
+                        Xplayer = true;
+                    }
+                }
+            }
+
+            //check row
+            for (int j = 0; j < 3; j++)
+            {
+                if ((arg[j, 0] == arg[j , 1]) && (arg[j, 1]== arg[j, 2]))
+                {
+                    if (arg[j, 2] == 0)
+                    {
+                        Oplayer = true;
+                    }
+                    else
+                    {
+                        Xplayer = true;
+                    }
+                }
+            }
+
+            //check diag
+            if ((arg[0,0] == arg[1,1]) && (arg[1,1] == arg[2, 2]))
+            {
+                if (arg[0,0] == 0)
+                {
+                    Oplayer = true;
+                }
+                else
+                {
+                    Xplayer = true;
+                }
+            }
+
+            if ((Oplayer == true) & (Xplayer==true))
+            {
+                return 2;
+            }else if (Oplayer)
+            {
+                return 0; //Player O winds
+            }
+            else
+            {
+                return 1; //Player X wins
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] arr = new int[3, 3];
+            int countX = 0;
+            int countO = 0;
+            Label[,] display_res = { {Lbx1,Lbx2, Lbx3},
+                                       {Lbx4,Lbx5, Lbx6 },
+                                       {Lbx7,Lbx8, Lbx9 } };
+            
+  
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    int seed = new Random().Next(); //more randomness
+
+                    arr[i,j] = new Random(i+j+seed).Next(0,2); //value is between 0 and 1
+
+                    if (arr[i,j] == 1) // 1 means player X
+                    {   if (countX >= 4) // 9 / 2 == 4.5
+                        {
+                            display_res[i, j].Text = "O";
+                            countO++;
+                        }
+                        else
+                        {
+                            display_res[i, j].Text = "X";
+                            countX++;
+                        }
+
+                    }
+                    else if (arr[i,j] == 0)//0 means player O
+                    {
+                        if (countO >= 4) // 9 / 2 = 4.5
+                        {
+                            display_res[i, j].Text = "X";
+                            countX++;
+                        }
+                        else
+                        {
+                            display_res[i, j].Text = "O";
+                            countO++;
+                        }
+
+                    }
+
+                }
+            }
+
+            if (win_check(arr) == 2)
+            {
+                ResTxb.Text = "The game is draw";
+            }else if (win_check(arr) == 0)
+            {
+                ResTxb.Text = "Player O wins";
+            }
+            else
+            {
+                ResTxb.Text = "Player X wins";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
